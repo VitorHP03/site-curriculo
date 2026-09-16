725ba130bf0d833008395db8b641884f91a10c7b
RAFAEL CARLOS PEREIRA <vitorhp0309@gmail.com>
2026-09-16 03:02:08 -0300

    corrigindo erro no javascript

--- script.js
+++ script.js
@@ -5,22 +5,19 @@ function rolarDireita() {
   let setaDireita = document.getElementById("seta-direita");
   let setaEsquerda = document.getElementById("seta-esquerda");
   let titulo = document.getElementById("tituloVideo");
-  num1++;
 
-  if (num1 === 1) {
-    video.src = "https://www.youtube.com/embed/abNwLjKFXDQ?si=b7-IQbqonCVtcmc5";
-    titulo.innerHTML = "Parte 1";
+  if (num1 < 3) {
+    num1++;
   }
   if (num1 === 2) {
     video.src = "https://www.youtube.com/embed/zkf3Az5TymQ?si=ga9wuAMfuYVelz06";
     titulo.innerHTML = "Parte 2";
-    setaEsquerda.src = "setas/esquerda.svg";
+    setaEsquerda.style.display = "inline-block";
     setaDireita.style.display = "inline-block";
-  }
-  if (num1 === 3) {
+  } else if (num1 === 3) {
     video.src = "https://www.youtube.com/embed/aZeIgqq6Scw?si=_RzV4-JufGWyZsSW";
     titulo.innerHTML = "Parte 3";
-    setaEsquerda.src = "setas/esquerda.svg";
+    setaEsquerda.style.display = "inline-block";
     setaDireita.style.display = "none";
   }
 }
@@ -31,23 +28,19 @@ function rolarEsquerda() {
   let setaEsquerda = document.getElementById("seta-esquerda");
   let titulo = document.getElementById("tituloVideo");
 
-  num1--;
+  if (num1 > 1) {
+    num1--;
+  }
 
   if (num1 === 1) {
     video.src = "https://www.youtube.com/embed/abNwLjKFXDQ?si=b7-IQbqonCVtcmc5";
     titulo.innerHTML = "Parte 1";
-    setaEsquerda.src = "";
-  }
-  if (num1 === 2) {
+    setaEsquerda.style.display = "none";
+    setaDireita.style.display = "inline-block";
+  } else if (num1 === 2) {
     video.src = "https://www.youtube.com/embed/zkf3Az5TymQ?si=ga9wuAMfuYVelz06";
     titulo.innerHTML = "Parte 2";
-    setaEsquerda.src = "setas/esquerda.svg";
+    setaEsquerda.style.display = "inline-block";
     setaDireita.style.display = "inline-block";
-  }
-  if (num1 === 3) {
-    video.src = "https://www.youtube.com/embed/aZeIgqq6Scw?si=_RzV4-JufGWyZsSW";
-    titulo.innerHTML = "Parte 3";
-    setaEsquerda.src = "setas/esquerda.svg";
-    setaDireita.style.display = "none";
   }
 }
